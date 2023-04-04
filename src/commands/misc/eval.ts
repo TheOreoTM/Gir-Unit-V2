8ee25020ed6e723bbf868743b818621a3963be5f
TheOreoTM <qwertzybad@gmail.com>
2023-04-04 17:42:43 +0500

    events + command made

--- src/commands/misc/eval.ts
+++ src/commands/misc/eval.ts
@@ -1,9 +1,5 @@
 import { Args, Command } from '@sapphire/framework';
-import { send } from '@sapphire/plugin-editable-commands';
-import { Type } from '@sapphire/type';
-import { codeBlock, isThenable } from '@sapphire/utilities';
-import type { Message } from 'discord.js';
-import { inspect } from 'util';
+import { EmbedBuilder, Message } from 'discord.js';
 
 export class UserCommand extends Command {
   public constructor(context: Command.Context, options: Command.Options) {
@@ -19,63 +15,47 @@ export class UserCommand extends Command {
   }
 
   public async messageRun(message: Message, args: Args) {
-    const code = await args.rest('string');
-
-    const { result, success, type } = await this.eval(code, {
-      async: args.getFlags('async'),
-      depth: Number(args.getOption('depth')) ?? 0,
-      showHidden: args.getFlags('hidden', 'showHidden'),
-    });
-
-    const output = success
-      ? codeBlock('js', result)
-      : `**ERROR**: ${codeBlock('bash', result)}`;
-    if (args.getFlags('silent', 's')) return null;
-
-    const typeFooter = `**Type**: ${codeBlock('typescript', type)}`;
-
-    if (output.length > 2000) {
-      return send(message, {
-        content: `Output was too long... sent the result as a file.\n\n${typeFooter}`,
-        files: [{ attachment: Buffer.from(output), name: 'output.js' }],
-      });
-    }
-
-    return send(message, `${output}\n${typeFooter}`);
-  }
-
-  private async eval(
-    code: string,
-    flags: { async: boolean; depth: number; showHidden: boolean }
-  ) {
-    if (flags.async) code = `(async () => {\n${code}\n})();`;
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-
-    let success = true;
-    let result = null;
-
+    const result = await args.rest('string');
     try {
-      // eslint-disable-next-line no-eval
-      result = eval(code);
-    } catch (error) {
-      if (error && error instanceof Error && error.stack) {
-        this.container.client.logger.error(error);
-      }
-      result = error;
-      success = false;
-    }
-
-    const type = new Type(result).toString();
-    if (isThenable(result)) result = await result;
-
-    if (typeof result !== 'string') {
-      result = inspect(result, {
-        depth: flags.depth,
-        showHidden: flags.showHidden,
+      let noResultArg = new EmbedBuilder()
+        .setColor('#e31212')
+        .setDescription('ERROR: No valid eval args were provided');
+      if (!result)
+        return message.channel.send({
+          embeds: [noResultArg],
+        });
+      let evaled = eval(result);
+      const resultSuccess = new EmbedBuilder()
+        .setColor('#8f82ff')
+        .setTitle('Success')
+        .addFields(
+          {
+            name: 'Input',
+            value: '```ts\n' + `${result}` + '```',
+            inline: false,
+          } || undefined,
+          {
+            name: 'Output',
+            value: '```ts\n' + `${evaled}` + '```',
+            inline: false,
+          } || undefined
+        );
+      await message.channel.send({
+        embeds: [resultSuccess],
       });
+      return;
+    } catch (error) {
+      const resultError =
+        new EmbedBuilder()
+          .setColor('#e31212')
+          .setTitle('An error has occured')
+          .setDescription(
+            `**Output:**\n\`\`\`${error}\`\`\` \n  Input:\n\`\`\`js\n ${result} \`\`\` `
+          ) || undefined;
+      await message.channel.send({
+        embeds: [resultError],
+      });
+      return;
     }
-
-    return { result, success, type };
   }
 }
